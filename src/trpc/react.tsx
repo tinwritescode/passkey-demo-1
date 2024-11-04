5c68abeff56915503686ff16f618c1efec3c928e
Tin Nguyen <tinnguyen@Tins-MacBook-Pro.local>
2024-11-04 14:05:25 +0700

    show logs

--- src/trpc/react.tsx
+++ src/trpc/react.tsx
@@ -45,9 +45,9 @@ export function TRPCReactProvider(props: { children: React.ReactNode }) {
     api.createClient({
       links: [
         loggerLink({
-          enabled: (op) =>
-            // process.env.NODE_ENV === "development" ||
-            op.direction === "down" && op.result instanceof Error,
+          enabled: (op) => true,
+          // process.env.NODE_ENV === "development" ||
+          // (op.direction === "down" && op.result instanceof Error),
         }),
 
         unstable_httpBatchStreamLink({
