391e576c46c6aab5698a4c478231e2dc073efa13
Tin Nguyen <tinnguyen@Tins-MacBook-Pro.local>
2024-11-04 13:07:20 +0700

    enable log

--- src/trpc/react.tsx
+++ src/trpc/react.tsx
@@ -46,8 +46,8 @@ export function TRPCReactProvider(props: { children: React.ReactNode }) {
       links: [
         loggerLink({
           enabled: (op) =>
-            process.env.NODE_ENV === "development" ||
-            (op.direction === "down" && op.result instanceof Error),
+            // process.env.NODE_ENV === "development" ||
+            op.direction === "down" && op.result instanceof Error,
         }),
 
         unstable_httpBatchStreamLink({
